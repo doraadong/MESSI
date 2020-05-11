a36fbef0d04fd04ce4ac32f59f6ddebf4b25ff24
Dora Li <dongshunzi.c@gmail.com>
2020-05-11 15:56:30 -0400

    Update utils.py

--- messi/utils.py
+++ messi/utils.py
@@ -3,22 +3,29 @@ import numpy as np
 import math
 
 def is_pos_def(x):
-        return np.all(np.linalg.eigvals(x) > 0)
+    return np.all(np.linalg.eigvals(x) > 0)
     
 def get_multigaussian_pdf(_mean, _cov, _cov_i, num_variable, Y_variable):
     """
     calculate multivariate Gaussian PDF for given mean & cov for each sample.
 
+    Parameters
+    ----------
+        _mean: numpy array
+            N x num_variable (N: sample size), mean
+        _cov: numpy array
+            num_variable x num_variable, covariance matrix
+        _cov_i: numpy array
+            num_variable x num_variable, inverse of the covariance matrix
+        num_variable: integer
+            number of variables
+        Y_variable: numpy array
+            N x num_variable, realized values of the random variables
 
-    Args:
-        _mean: numpy array, N x num_variable (N: sample size), mean
-        _cov: numpy array, num_variable x num_variable, covariance matrix
-        _cov_i: numpy array, num_variable x num_variable, inverse of the covariance matrix
-        num_variable: integer, number of variables
-        Y_variable: numpy array, N x num_variable, realized values of the random variables
-
-    Returns:
-        list of likelihoods for every sample
+    Returns
+    -------
+        likelihoods: list of float
+            list of likelihoods for every sample
 
     """
 
@@ -32,4 +39,5 @@ def get_multigaussian_pdf(_mean, _cov, _cov_i, num_variable, Y_variable):
     return _nominator/_denominator
 
 flatten = lambda l: [item for sublist in l for item in sublist]
+
 
